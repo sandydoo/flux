22063eac639b4c6a9a92a55c0bfb6ab1ad57bbc4
sandydoo <hey@sandydoo.me>
2021-12-10 04:22:17 +0300

    Bundle CNAME when building the app
    This is for GitHub Pages to work with custom domains.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
-const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './index.js',
@@ -14,7 +15,12 @@ module.exports = {
       template: 'index.html',
     }),
     new WasmPackPlugin({
-      crateDirectory: path.resolve(__dirname, "."),
+      crateDirectory: path.resolve(__dirname, '.'),
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'public' },
+      ],
     }),
   ],
   mode: 'development',
