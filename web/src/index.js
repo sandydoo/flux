ebe6b979197eb1781d12daf4014880ac8c494633
sandydoo <hey@sandydoo.me>
2022-02-10 00:22:12 +0300

    Initialize the web version of flux once the DOM is ready

--- web/src/index.js
+++ web/src/index.js
@@ -1,35 +1,36 @@
-import  { Flux } from '../flux';
-import { Elm } from './Main.elm';
+import  { Flux } from "../flux";
+import { Elm } from "./Main.elm";
 
+function setupFlux() {
+  let flux;
 
-let flux;
-
-// Set up Elm UI
-const ui = Elm.Main.init({
-  node: document.getElementById('controls'),
-});
-
-// Initialize WASM and run animation
-ui.ports.initFlux.subscribe(function (settings) {
-  flux = new Flux(settings);
-
-  function animate(timestamp) {
-    flux.animate(timestamp);
-    window.requestAnimationFrame(animate);
-  }
-
-  const resizeObserver = new ResizeObserver(([entry]) => {
-    let { width, height } = entry.contentRect;
-    flux.resize(width, height);
+  // Set up Elm UI
+  const ui = Elm.Main.init({
+    node: document.getElementById("controls"),
   });
-  resizeObserver.observe(document.getElementById("canvas"));
 
-  window.requestAnimationFrame(animate);
+  // Initialize WASM and run animation
+  ui.ports.initFlux.subscribe(function (settings) {
+    flux = new Flux(settings);
 
+    function animate(timestamp) {
+      flux.animate(timestamp);
+      window.requestAnimationFrame(animate);
+    }
 
-});
+    const resizeObserver = new ResizeObserver(([entry]) => {
+      let { width, height } = entry.contentRect;
+      flux.resize(width, height);
+    });
+    resizeObserver.observe(document.getElementById("canvas"));
 
-// Update settings
-ui.ports.setSettings.subscribe(function (newSettings) {
-  flux.settings = newSettings;
-});
+    window.requestAnimationFrame(animate);
+  });
+
+  // Update settings
+  ui.ports.setSettings.subscribe(function (newSettings) {
+    flux.settings = newSettings;
+  });
+}
+
+window.addEventListener("DOMContentLoaded", setupFlux());
