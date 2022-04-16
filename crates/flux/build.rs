01f3d810f9eef41310b238b7b50a7c8239cb4c43
sandydoo <hey@sandydoo.me>
2022-04-16 19:33:38 +0400

    Fix shader compilation on Windows
    `sampler2D` is not a valid type when setting precision in core330.
    It is in the ES version though, which makes things very confusing. The
    proper solution here is to stop writing GLSL directly and instead
    write either SPIR-V or WGSL, compiling to whichever GLSL version we
    need with naga.

--- crates/flux/build.rs
+++ crates/flux/build.rs
@@ -7,9 +7,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     let target = env::var("TARGET").unwrap();
     let shaders_files = fs::read_dir("shaders")?;
 
+    // TODO: rewrite the shaders to either WGSL or SPIR-V, and the compile to
+    // whichever target we need.
     let version = match target.as_str() {
-        "wasm32-unknown-unknown" => "#version 300 es\n",
-        _ => "#version 330\n",
+        "wasm32-unknown-unknown" => "300 es",
+        "aarch64-apple-darwin" | "x86_64-apple-darwin" => "330",
+        _ => "460", // `precision sampler2D` was added to core much later
     };
 
     for shader_file in shaders_files {
@@ -17,7 +20,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             let path = entry.path();
             let mut shader_source = File::open(&path)?;
 
-            let mut version_shader_source = String::new() + version;
+            let mut version_shader_source = format!("#version {}\n", version);
             shader_source.read_to_string(&mut version_shader_source)?;
 
             let dest_path = Path::new(&out_dir).join(&path);
