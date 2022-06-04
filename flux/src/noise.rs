f1358121455fb1f868b9da9574bc88016ea24132
sandydoo <hey@sandydoo.me>
2022-06-04 22:59:59 +0400

    Reset noise offsets when they get too large

--- flux/src/noise.rs
+++ flux/src/noise.rs
@@ -21,8 +21,8 @@ pub struct NoiseUniforms {
     scale: f32,
     offset_1: f32,
     offset_2: f32,
-    multiplier: f32,
     blend_factor: f32,
+    multiplier: f32,
 }
 
 pub struct NoiseChannel {
@@ -30,17 +30,27 @@ pub struct NoiseChannel {
     scale: f32,
     offset_1: f32,
     offset_2: f32,
-    blend_begin_time: f32,
-    last_blend_progress: f32,
+    blend_factor: f32,
 }
 
 impl NoiseChannel {
     pub fn tick(&mut self, elapsed_time: f32) -> () {
-        self.blend_begin_time = elapsed_time;
-        self.last_blend_progress = 0.0;
+        const BLEND_THRESHOLD: f32 = 20.0;
 
         self.scale = self.settings.scale + 0.3 * (0.001 * elapsed_time.to_radians()).sin();
         self.offset_1 += self.settings.offset_increment;
+
+        if self.offset_1 > BLEND_THRESHOLD {
+            self.blend_factor += self.settings.offset_increment;
+            self.offset_2 += self.settings.offset_increment;
+        }
+
+        // Reset blending
+        if self.blend_factor > 1.0 {
+            self.offset_1 = self.offset_2;
+            self.offset_2 = 0.0;
+            self.blend_factor = 0.0;
+        }
     }
 }
 
@@ -143,10 +153,9 @@ impl NoiseGeneratorBuilder {
         self.channels.push(NoiseChannel {
             settings: channel.clone(),
             scale: channel.scale,
-            offset_1: 100.0 * rand::random::<f32>(),
+            offset_1: 2.0 * rand::random::<f32>(),
             offset_2: 0.0,
-            blend_begin_time: 0.0,
-            last_blend_progress: 0.0,
+            blend_factor: 0.0,
         });
 
         self
@@ -237,8 +246,8 @@ fn build_noise_uniforms(channels: &[NoiseChannel]) -> Vec<u8> {
             scale: channel.scale,
             offset_1: channel.offset_1,
             offset_2: channel.offset_2,
+            blend_factor: channel.blend_factor,
             multiplier: channel.settings.multiplier,
-            blend_factor: 0.0,
         })
         .collect();
     let mut aligned_uniforms = Vec::new();
