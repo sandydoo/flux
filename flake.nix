16e2d56677f69d614296f3346873d5bf6c370c64
sandydoo <hey@sandydoo.me>
2023-01-17 03:17:42 +0400

    Bump dependencies
    Except for glutin.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     flake-utils.url = "github:numtide/flake-utils";
     crane = {
       url = "github:ipetkov/crane";
