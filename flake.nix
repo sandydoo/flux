a91f8bfd26c32fa9b8360c386d4c87e0b088af2c
Sander <hey@sandydoo.me>
2023-07-12 11:33:34 +0000

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'crane':
        'github:ipetkov/crane/0c9f468ff00576577d83f5019a66c557ede5acf6' (2023-04-11)
      → 'github:ipetkov/crane/8b08e96c9af8c6e3a2b69af5a7fa168750fcf88e' (2023-07-07)
    • Updated input 'crane/flake-utils':
        'github:numtide/flake-utils/93a2b84fc4b70d9e089d029deacc3583435c2ed6' (2023-03-15)
      → 'github:numtide/flake-utils/dbabf0ca0c0c4bce6ea5eaf65af5cb694d2082c7' (2023-06-25)
    • Added input 'crane/flake-utils/systems':
        'github:nix-systems/default/da67096a3b9bf56a91d16901293e51ba5b49a27e' (2023-04-09)
    • Updated input 'flake-utils':
        'github:numtide/flake-utils/cfacdce06f30d2b68473a46042957675eebb3401' (2023-04-11)
      → 'github:numtide/flake-utils/919d646de7be200f3bf08cb76ae1f09402b6f9b4' (2023-07-11)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/fda0d99c2cbbb5c89d8855d258cb0821bd9113ad' (2023-04-14)
      → 'github:NixOS/nixpkgs/8163a64662b43848802092d52015ef60777d6129' (2023-07-11)
    • Updated input 'nixpkgs-master':
        'github:NixOS/nixpkgs/2fee028681cd0caa48a2aeb75f5c42fd50e4f298' (2023-04-15)
      → 'github:NixOS/nixpkgs/5efffcd7d77fb1b8be9199bb2aa7816796c72c31' (2023-07-12)
    • Updated input 'rust-overlay':
        'github:oxalica/rust-overlay/b6f8d87208336d7cb85003b2e439fc707c38f92a' (2023-04-15)
      → 'github:oxalica/rust-overlay/db8d909c9526d4406579ee7343bf2d7de3d15eac' (2023-07-12)

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     nixpkgs-master.url = "github:NixOS/nixpkgs/master";
     flake-utils.url = "github:numtide/flake-utils";
     crane = {
