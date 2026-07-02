bd6faace3db1788b99fab153570ef4e898ccf931
Sander <hey@sandydoo.me>
2026-07-02 18:17:08 +0200

    desktop: remove custom seed

--- flux-desktop/src/main.rs
+++ flux-desktop/src/main.rs
@@ -197,10 +197,7 @@ impl ApplicationHandler for FluxApp {
         window_surface.configure(&device, &config);
 
         let logical_size = physical_size.to_logical(window.scale_factor());
-        let settings = Arc::new(Settings {
-            seed: Some("1337".into()),
-            ..Default::default()
-        });
+        let settings = Arc::new(Settings::default());
         let flux = Flux::new(
             &device,
             &command_queue,
